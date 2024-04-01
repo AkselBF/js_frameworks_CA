b4ff70f515607100d272e73841dbd40528bdd178
AkselBF <aksel.boutrouefaret@yahoo.com>
2024-04-01 06:32:01 +0200

    fixing product page

--- src/common/pages/Product/index.tsx
+++ src/common/pages/Product/index.tsx
@@ -40,21 +40,20 @@ const Product: React.FC<ProductProps> = ({ addToCart }) => {
     <div className='justify-center md:w-[90%]'>
       {product ? (
         <div className='mx-auto w-full lg:w-[90%]'>
-          <h1 className='text-3xl font-bold mx-auto my-5 underline'>{product.title}</h1>
-          <div className='bg-[#171717] px-5 py-10 md:pr-10 md:pl-5 mx-auto flex flex-col md:flex-row md:rounded-r-xl'>
+          <h1 className='text-3xl font-bold mx-5 lg:mx-auto my-5 underline'>{product.title}</h1>
+          <div className='bg-[#171717] px-5 pt-10 pb-10 md:pb-24 lg:pb-10 md:pr-10 md:pl-5 mx-auto flex flex-col md:flex-row md:rounded-r-xl'>
             <img src={product.image.url} alt={product.image.alt}
             className='w-[300px] mx-auto md:mx-10 rounded-lg' />
             <div className='relative text-white w-[80%] mx-auto'>
               <p className='mt-5 md:mt-0 mb-3'>{product.description}</p>
-              <p className='font-semibold'>Price: {product.discountedPrice}kr</p>
-              <p className='my-6 md:line-clamp-3'>
+              <p className='text-[#FF8A00] font-semibold'>Discount: {product.discountedPrice}kr</p>
+              <p className='mt-6 mb-12'>
                 If the item is to your liking, click the add to cart to add it in the cart or purchase button to purchase this one item directly.
-                We hope you enjoy this purchase and any future ones you may have.
               </p>
-              <div className='w-[80%] my-8 flex flex-col sm:mx-auto md:absolute md:-bottom-8 md:right-0 lg:-right-20 xl:-right-36'>
+              <div className='flex flex-col md:flex-row sm:mx-auto md:absolute right-0 bottom-0 md:-bottom-20 lg:-bottom-3'>
                 <button onClick={handleAddToCart} 
-                className='text-black font-semibold bg-[#80A4FF] mx-auto w-[200px] py-2 mb-2 rounded-md'>Add to Cart</button>
-                <Link to={'/checkout'} className='mx-auto mt-2'>
+                className='text-black font-semibold bg-[#80A4FF] mx-auto md:mx-3 w-[200px] py-2 mb-2 rounded-md'>Add to Cart</button>
+                <Link to={'/checkout'} className='mx-auto mt-2 md:mt-0'>
                   <button className='text-black font-semibold bg-[#FF8A00] w-[200px] py-2 rounded-md'>Checkout</button>
                 </Link>
               </div>
