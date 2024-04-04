5baab1f56c35eb412c4416f534ead2a8be4541fd
AkselBF <aksel.boutrouefaret@yahoo.com>
2024-04-04 14:31:33 +0200

    finishing touches

--- src/common/pages/Cart/index.tsx
+++ src/common/pages/Cart/index.tsx
@@ -1,38 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../images/ecom_logo.png';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 interface CartProps {
   cart: any[];
   removeFromCart: (index: number) => void;
+  updateItemQuantity: (index: number, quantity: number) => void;
 }
 
-const Cart: React.FC<CartProps> = ({ cart, removeFromCart }) => {
-  const totalPrice = cart.reduce((total, item) => total + item.discountedPrice, 0).toFixed(2);
+const Cart: React.FC<CartProps> = ({ cart, removeFromCart, updateItemQuantity }) => {
+  const totalPrice = cart.reduce((total, item) => total + item.discountedPrice * item.quantity, 0).toFixed(2);
 
-  return (
+  return ( 
     <div className='w-full p-5'>
       <h1 className='text-3xl text-center font-bold mb-5'>My cart</h1>
       <div className='flex flex-col lg:flex-row sm:justify-between'>
         <div className='lg:w-[45%]'>
           <ul className='my-5 lg:max-h-[410px] overflow-y-auto'>
             {cart.length === 0 ? (
-              <li className='text-gray-500'>No items</li>
+              <div className='justify-center text-center sm:text-left'>
+                <li className='text-gray-500 text-left mb-5'>Please select a product from the home page</li>
+                <Link to="/">
+                  <button className='text-white bg-[#171717] py-1.5 w-[200px] rounded-md my-3 md:my-0'>Back to home</button>
+                </Link>
+              </div>
             ) : (
               cart.map((item, index) => (
-                <li key={index} className='my-5 flex flex-col sm:flex-row sm:justify-between'>
+                <li key={index} className='my-5 flex flex-col sm:flex-row justify-between'>
                   <div className='flex flex-row'>
-                    <img src={item.image.url} alt={item.image.alt} 
-                    className='w-10 h-12 rounded-md' />
+                    <img src={item.image.url} alt={item.image.alt} className='w-10 h-12 rounded-md' />
                     <div className='mx-5'>
                       <h2 className='font-semibold'>{item.title}</h2>
-                      <p className='text-[#FF8A00]'>Discount: {item.discountedPrice}kr</p>
+                      <p className='text-[#FF8A00]'>Price: {item.discountedPrice}kr</p>
                     </div>
                   </div>
-                  <button onClick={() => removeFromCart(index)} 
-                  className='mx-0 md:mx-5 my-3 sm:my-0 bg-red-600 text-white font-semibold rounded-md py-2 w-[160px]'>
-                    Delete
-                  </button>
+                  <div className='flex items-center'>
+                    <button onClick={() => updateItemQuantity(index, item.quantity - 1)} className='text-black font-bold bg-[#80A4FF] mx-2 px-3 py-1 rounded-full'>-</button>
+                    <span>{item.quantity}</span>
+                    <button onClick={() => updateItemQuantity(index, item.quantity + 1)} className='text-black font-bold bg-[#80A4FF] mx-2 px-2.5 py-1 rounded-full'>+</button>
+                    <DeleteIcon onClick={() => removeFromCart(index)} className='mx-0 md:mx-5 my-5 sm:my-0 text-red-600 cursor-pointer' />
+                  </div>
                 </li>
               ))
             )}
@@ -46,9 +54,9 @@ const Cart: React.FC<CartProps> = ({ cart, removeFromCart }) => {
           <p className='my-5'>
             With that, we wish you happy shopping!
           </p>
-          <h4 className='text-right text-[#FF8A00] font-semibold'>Total Price: {totalPrice}kr</h4>
+          <h4 className='text-right text-[#FF8A00] font-semibold'>Total: {totalPrice}kr</h4>
           <div className='relative my-28'>
-            {cart.length > 0 ? (
+            {cart.length > 0 && totalPrice != 0 ? (
               <Link to={'/checkout'}>
                 <button className='absolute bottom-10 right-0 text-black font-semibold bg-[#00B2FF] py-2 w-[180px] rounded-md'>Checkout</button>
               </Link>
